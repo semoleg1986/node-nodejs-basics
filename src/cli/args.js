c4a4dcc4ca52fe9a1c2e355cbc99f59a279a4e99
Oleg Semenov <semoleg1986@gmail.com>
2023-06-14 23:30:34 +0300

    feat: update args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
-};
-
-parseArgs();
+    const args = process.argv.slice(2);
+    const parsedArgs = {};
+  
+    for (let i = 0; i < args.length; i += 2) {
+      const propName = args[i].slice(2);
+      const value = args[i + 1];
+      parsedArgs[propName] = value;
+    }
+  
+    for (const propName in parsedArgs) {
+      const value = parsedArgs[propName];
+      console.log(`${propName} is ${value}`);
+    }
+  };
+  
+  parseArgs();
+  
