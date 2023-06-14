03bb596f0c9f217a16b22deeb9a7f78ebc5741f2
Oleg Semenov <semoleg1986@gmail.com>
2023-06-14 23:29:01 +0300

    feat: update delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { promises as fs } from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+  const removeFile = 'fileToRemove.txt';
+  const dirName = 'files';
+
+  try {
+    const filePath = fileURLToPath(import.meta.url);
+    const directoryPath = path.dirname(filePath);
+    const filePathToDelete = path.join(directoryPath,dirName,removeFile);
+
+    await fs.unlink(filePathToDelete);
+    console.log('File removed successfully.');
+  } catch (err) {
+    console.error('FS operation failed');
+  }
 };
 
 await remove();
