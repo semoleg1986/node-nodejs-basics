d852de93dc97ad07796a11204010edd31fe9ce7d
Oleg Semenov <semoleg1986@gmail.com>
2023-06-14 23:35:18 +0300

    feat: update env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
-};
-
-parseEnv();
+    const prefix = 'RSS_';
+  
+    const parsedVariables = Object.entries(process.env)
+      .filter(([key]) => key.startsWith(prefix))
+      .map(([key, value]) => `${key}=${value}`);
+  
+    const result = parsedVariables.join('; ');
+    console.log(result);
+  };
+  
+  parseEnv();
+  
