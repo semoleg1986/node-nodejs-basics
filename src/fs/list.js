66907d039eedb6afbb68fdecc124d6889cdfa15a
Oleg Semenov <semoleg1986@gmail.com>
2023-06-14 23:29:46 +0300

    feat: update list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { promises as fs, readdir } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const dirName = 'files';
+    const filePath = fileURLToPath(import.meta.url);
+    const directoryPath = path.dirname(filePath);
+    const filePathToPrint = path.join(directoryPath, dirName);
+    try {
+        const files = await fs.readdir(filePathToPrint);
+        console.log(files);
+    } catch(err){
+        console.log('FS operation failed')
+    }
+
 };
 
 await list();
