99a332e1fe971c3ca3c9b9f401a8c936476b355f
Oleg Semenov <semoleg1986@gmail.com>
2023-06-15 00:08:32 +0300

    feat: update calcHash

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const folder = 'files'
+    const file = 'fileToRead.txt'
+
+    const filePath = fileURLToPath(import.meta.url);
+    const directoryPath = path.dirname(filePath);
+    const filePathToRead = path.join(directoryPath, folder, file);
+    const readStream = createReadStream(filePathToRead, { encoding: 'utf-8' });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('error', (error) => {
+        console.error('Error reading the file:', error);
+    });
+
+    readStream.on('end', () => {
+        console.log('File read operation completed');
+    });
 };
 
 await read();
