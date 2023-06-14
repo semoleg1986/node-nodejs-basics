92e4942af1397201329a604683d146f9091ac4a7
Oleg Semenov <semoleg1986@gmail.com>
2023-06-14 23:28:33 +0300

    feat: update create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const dirName = 'files';
+  const text = 'I am fresh and young';
+
+  const filePath = fileURLToPath(import.meta.url);
+  const directoryPath = path.dirname(filePath);
+  const filePathToCreate = path.join(directoryPath, dirName, fileName);
+
+  try {
+    await fs.writeFile(filePathToCreate, text, { flag: 'wx' });
+    console.log('FS operation successfully.');
+  } catch (err) {
+    console.error('FS operation failed');
+  }
 };
 
 await create();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { promises as fs, readdir } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 const read = async () => {
-    // Write your code here 
+    const dirName = 'files';
+    const readFile = 'fileToRead.txt'
+    try {     
+        const filePath = await fileURLToPath(import.meta.url);
+        const directoryPath = await path.dirname(filePath);
+        const filePathToRead= await path.join(directoryPath, dirName ,readFile);
+        const data = await fs.readFile(filePathToRead, 'utf-8');
+        console.log(data);
+    }catch (err) {
+        console.error('FS operation failed');
+      }
+
+
 };
 
 await read();
